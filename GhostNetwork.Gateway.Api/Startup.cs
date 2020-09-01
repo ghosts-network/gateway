0ae0ae9dbbbc067fc1afd955e5b3c3afedbe587c
vladimirborodin <boberneprotiv@gmail.com>
2020-09-01 16:52:29 +0300

    Init api project

---
+++ GhostNetwork.Gateway.Api/Startup.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GhostNetwork.Gateway.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
