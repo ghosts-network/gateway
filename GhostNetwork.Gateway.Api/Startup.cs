04c2c16cdeb212a224b5f6af6a1b6d0c5e3b2772
vladimirborodin <boberneprotiv@gmail.com>
2020-09-09 19:29:10 +0300

    Configure base structure of api

--- GhostNetwork.Gateway.Api/Startup.cs
+++ GhostNetwork.Gateway.Api/Startup.cs
@@ -1,8 +1,10 @@
+using GhostNetwork.Publications.Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace GhostNetwork.Gateway.Api
 {
@@ -17,15 +19,38 @@ namespace GhostNetwork.Gateway.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddCors();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "GhostNetwork/Gateway API",
+                    Version = "1.0.0"
+                });
+            });
+
+            services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                
+                app
+                    .UseSwagger()
+                    .UseSwaggerUI(config =>
+                    {
+                        config.SwaggerEndpoint("/swagger/v1/swagger.json", "Gateway V1");
+                    });
+
+                app.UseCors(builder => builder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin());
             }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
