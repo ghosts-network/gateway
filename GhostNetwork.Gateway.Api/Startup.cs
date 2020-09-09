6010cae61ed58bc7fd1d149e3dea66fab67873b6
vladimirborodin <boberneprotiv@gmail.com>
2020-09-09 19:23:56 +0300

    Remove graphql

--- GhostNetwork.Gateway.Api/Startup.cs
+++ GhostNetwork.Gateway.Api/Startup.cs
@@ -1,9 +1,5 @@
-using GhostNetwork.Gateway.Api.GraphQL;
-using GraphQL.Server;
-using GraphQL.Server.Ui.GraphiQL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,30 +17,15 @@ namespace GhostNetwork.Gateway.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<KestrelServerOptions>(options =>
-            {
-                options.AllowSynchronousIO = true;
-            });
-            services
-                .AddSingleton<GhostNetworkSchema>()
-                .AddSingleton<GhostNetworkQuery>()
-                .AddSingleton<GhostNetworkQuery>()
-                .AddSingleton<PublicationType>()
-                .AddGraphQL(options =>
-                {
-                    options.EnableMetrics = true;
-                    options.ExposeExceptions = true;
-                });
+            
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseGraphiQLServer(new GraphiQLOptions());
+                
             }
-
-            app.UseGraphQL<GhostNetworkSchema>();
         }
     }
 }
