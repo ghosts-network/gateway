cce017ac6fda15087aeda259d9caf78585d9d64f
Alexander <50077233+Sekir0@users.noreply.github.com>
2021-01-04 15:59:42 +0200

    rmv null check

--- Infrastructure/GhostNetwork.Infrastructure.Repository/NewsFeedStorage.cs
+++ Infrastructure/GhostNetwork.Infrastructure.Repository/NewsFeedStorage.cs
@@ -119,18 +119,12 @@ namespace GhostNetwork.Infrastructure.Repository
 
         public async Task AddReactionAsync(string publicationId, string author, ReactionType reaction)
         {
-            if (author != null)
-            {
-                await reactionsApi.ReactionsAddAsync($"publication_{publicationId}", reaction.ToString(), author);
-            }
+            await reactionsApi.ReactionsAddAsync($"publication_{publicationId}", reaction.ToString(), author);
         }
 
         public async Task RemoveReactionAsync(string publicationId, string author)
         {
-            if (author != null)
-            {
-                await reactionsApi.ReactionsDeleteAsync($"publication_{publicationId}", author);
-            }
+            await reactionsApi.ReactionsDeleteAsync($"publication_{publicationId}", author);
         }
 
         public async Task UpdateAsync(string id, string content)
