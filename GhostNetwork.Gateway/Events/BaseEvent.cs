541e42fd4dcd16886a1c4e8f6f980b82cb8e32fc
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-07-07 03:04:19 +0300

    add event models

---
+++ GhostNetwork.Gateway/Events/BaseEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostNetwork.Gateway.Events
+{
+    public abstract class BaseEvent
+    {
+        public string TriggeredBy { get; protected set; }
+    }
+}
---
+++ GhostNetwork.Gateway/Events/ProfileChangedEvent.cs
@@ -0,0 +1,15 @@
+﻿using GhostNetwork.Gateway.Users;
+
+namespace GhostNetwork.Gateway.Events
+{
+    public class ProfileChangedEvent : BaseEvent
+    {
+        public User UpdatedUser { get; }
+
+        public ProfileChangedEvent(string userTrigeredBy, User updatedUser)
+        {
+            TriggeredBy = userTrigeredBy;
+            UpdatedUser = updatedUser;
+        }
+    }
+}
