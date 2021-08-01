9b8b04f46f97f50168218fde830886b197f21ad1
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-08-01 22:12:37 +0300

    refactoring events

---
+++ GhostNetwork.Gateway.RedisMq/Events/EventBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GhostNetwork.Gateway.RedisMq.Events
+{
+    public abstract class EventBase
+    {
+        public string TriggeredBy { get; init; }
+        public DateTimeOffset CreatedOn { get; } = DateTimeOffset.UtcNow;
+    }
+}
---
+++ GhostNetwork.Gateway.RedisMq/Events/ProfileChangedEvent.cs
@@ -0,0 +1,9 @@
+﻿using GhostNetwork.Gateway.Users;
+
+namespace GhostNetwork.Gateway.RedisMq.Events
+{
+    public class ProfileChangedEvent : EventBase
+    {
+        public User UpdatedUser { get; init; }
+    }
+}
