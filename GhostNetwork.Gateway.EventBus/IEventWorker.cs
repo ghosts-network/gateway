53d8c159ab612ec5a83a32c7429dfac694e2180e
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-08-25 19:06:25 +0300

    add description to IEventHandler

--- GhostNetwork.Gateway.EventBus/IEventWorker.cs
+++ GhostNetwork.Gateway.EventBus/IEventWorker.cs
@@ -3,10 +3,20 @@ using GhostNetwork.Gateway.EventBus.Events;
 
 namespace GhostNetwork.Gateway.EventBus
 {
+    /// <summary>
+    /// Exposes the functionality for starting event listening.
+    /// </summary>
     public interface IEventWorker
     {
+        /// <summary>
+        /// Starts listening Queue and run handler on event.
+        /// </summary>
+        /// <typeparam name="TEvent">The type of event that listens to.</typeparam>
         void Subscribe<TEvent>() where TEvent : EventBase, new();
-        
+        /// <summary>
+        /// Starts listening Queue and run handler on event.
+        /// </summary>
+        /// <param  name="type">The type of event that listens to represented by System.Type.</param>
         void Subscribe(string key, Type type);
     }
 }
