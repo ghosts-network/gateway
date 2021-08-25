dcbeaad96982784449f26e10b1ad2d43ccc75869
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-08-25 19:02:53 +0300

    add description for IEventHandler

--- GhostNetwork.Gateway.EventBus/IEventHandler.cs
+++ GhostNetwork.Gateway.EventBus/IEventHandler.cs
@@ -3,14 +3,26 @@ using GhostNetwork.Gateway.EventBus.Events;
 
 namespace GhostNetwork.Gateway.EventBus
 {
+    /// <summary>
+    /// Exposes the functionality to handle events.
+    /// </summary>   
     public interface IEventHandler 
     {
-
+        /// <summary>
+        /// Starts a handling of event represented by parent class of event-model.
+        /// </summary>   
         Task Handle(EventBase value);
     }
 
+    /// <summary>
+    /// Exposes the generic variant of functionality to handle events.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of event that needs to handle to.</typeparam>
     public interface IEventHandler<TEvent> : IEventHandler where TEvent : EventBase 
     {
+        /// <summary>
+        /// Starts a handling of event represented by generic-type.
+        /// </summary>   
         Task Handle(TEvent value);
     }
 }
