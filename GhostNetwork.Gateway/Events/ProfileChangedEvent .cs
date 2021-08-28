c1af6b91cee3c2a09a6e0e9e5fd971bf4ea62d0d
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-08-28 19:56:38 +0300

    Create ProfileChangedEvent .cs

---
+++ GhostNetwork.Gateway/Events/ProfileChangedEvent .cs
@@ -0,0 +1,10 @@
+﻿using GhostNetwork.Gateway.EventBus.Events;
+using GhostNetwork.Gateway.Users;
+
+namespace GhostNetwork.Gateway.Events
+{
+    public class ProfileChangedEvent : EventBase
+    {
+        public User User { get; init; }
+    }
+}
