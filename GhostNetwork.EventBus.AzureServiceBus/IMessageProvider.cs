fcbbde8d207eed46adc422501979b3b6dd377b6e
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-04-02 21:58:14 +0300

    Create IMessageProvider.cs

---
+++ GhostNetwork.EventBus.AzureServiceBus/IMessageProvider.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GhostNetwork.EventBus.AzureServiceBus
+{
+    public interface IMessageProvider
+    {
+        byte[] GetMessage<TEvent>(TEvent @event)
+            where TEvent : Event;
+
+        object GetEvent(byte[] message, Type outputType);
+    }
+}
