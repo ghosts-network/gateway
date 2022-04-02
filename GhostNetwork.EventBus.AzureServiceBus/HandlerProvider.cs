6bebe0d3d896d09b8985edf36e29eecbac1e1259
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-04-02 21:59:06 +0300

    Create HandlerProvider.cs

---
+++ GhostNetwork.EventBus.AzureServiceBus/HandlerProvider.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GhostNetwork.EventBus.AzureServiceBus
+{
+    public class HandlerProvider : IHandlerProvider
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public HandlerProvider(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IEventHandler<TEvent> GetRequiredService<TEvent>(Type type)
+            where TEvent : Event
+        {
+            var scope = serviceProvider.CreateScope();
+            return scope.ServiceProvider.GetRequiredService(type) as IEventHandler<TEvent>;
+        }
+    }
+}
