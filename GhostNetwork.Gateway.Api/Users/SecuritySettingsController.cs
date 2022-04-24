274a481b5cd1f4dad5ffb4918c82b0a31280ec4b
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-04-24 22:59:27 +0300

    implement access

---
+++ GhostNetwork.Gateway.Api/Users/SecuritySettingsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GhostNetwork.Gateway.Api.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecuritySettingsController : ControllerBase
+    {
+    }
+}
---
+++ GhostNetwork.Gateway/SecuritySettings/ISecuritySettingsStorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostNetwork.Gateway.SecuritySettings;
+
+public interface ISecuritySettingsStorage
+{
+}
