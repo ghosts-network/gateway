4114cbd5f904a121668d3f2d2fbcde4e3786e63e
dsm2136 <48469678+dsm2136@users.noreply.github.com>
2022-04-25 00:52:05 +0300

    Create ISecuritySettingStorage.cs

---
+++ GhostNetwork.Gateway/Users/ISecuritySettingStorage.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using GhostNetwork.Profiles.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace GhostNetwork.Gateway.Users
+{
+    public interface ISecuritySettingStorage
+    {
+        Task<SecuritySetting> FindByProfileAsync(Guid userId);
+
+        Task<DomainResult> UpdateAsync(Guid userId, SecuritySettingUpdateViewModel model);
+    }
+}
