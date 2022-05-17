03675d00042c2552ab2f3ef91dcaf46dcef43c42
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-05-17 08:47:48 +0300

    Update SecuritySetting.cs

--- GhostNetwork.Gateway/Users/SecuritySection/SecuritySetting.cs
+++ GhostNetwork.Gateway/Users/SecuritySection/SecuritySetting.cs
@@ -8,23 +8,39 @@ namespace GhostNetwork.Gateway.Users
         public SecuritySetting(
             Guid userId,
             SecuritySettingSection posts,
-            SecuritySettingSection friends)
+            SecuritySettingSection friends,
+            SecuritySettingSection followers,
+            SecuritySettingSection comments,
+            SecuritySettingSection profilePhoto)
         {
             UserId = userId;
             Posts = posts;
             Friends = friends;
+            Followers = followers;
+            Comments = comments;
+            ProfilePhoto = profilePhoto;
         }
 
         public Guid UserId { get; }
 
         public SecuritySettingSection Posts { get; private set; }
 
+        public SecuritySettingSection Followers { get; private set; }
+
         public SecuritySettingSection Friends { get; private set; }
+
+        public SecuritySettingSection Comments { get; private set; }
+
+        public SecuritySettingSection ProfilePhoto { get; private set; }
+
 
         public static SecuritySetting DefaultForUser(Guid userId)
         {
             return new SecuritySetting(
                 userId,
+                new SecuritySettingSection(Access.Everyone, Enumerable.Empty<Guid>()),
+                new SecuritySettingSection(Access.Everyone, Enumerable.Empty<Guid>()),
+                new SecuritySettingSection(Access.Everyone, Enumerable.Empty<Guid>()),
                 new SecuritySettingSection(Access.Everyone, Enumerable.Empty<Guid>()),
                 new SecuritySettingSection(Access.Everyone, Enumerable.Empty<Guid>()));
         }
