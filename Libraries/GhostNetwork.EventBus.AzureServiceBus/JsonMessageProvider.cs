7c040a54ca315aac076611afc7d1c074f5b70b6d
boberneprotiv <boberneprotiv@gmail.com>
2022-05-31 13:35:29 +0300

    Fix json formatting for AzureServiceBus

--- Libraries/GhostNetwork.EventBus.AzureServiceBus/JsonMessageProvider.cs
+++ Libraries/GhostNetwork.EventBus.AzureServiceBus/JsonMessageProvider.cs
@@ -6,15 +6,21 @@ namespace GhostNetwork.EventBus.AzureServiceBus
 {
     public class JsonMessageProvider : IMessageProvider
     {
+        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+        
         public byte[] GetMessage<TEvent>(TEvent @event)
             where TEvent : Event
         {
-            return Encoding.UTF8.GetBytes(JsonSerializer.Serialize(@event));
+            return Encoding.UTF8.GetBytes(JsonSerializer.Serialize(@event, Options));
         }
 
         public object GetEvent(byte[] message, Type outputType)
         {
-            return JsonSerializer.Deserialize(Encoding.UTF8.GetString(message), outputType);
+            return JsonSerializer.Deserialize(Encoding.UTF8.GetString(message), outputType, Options);
         }
     }
 }
