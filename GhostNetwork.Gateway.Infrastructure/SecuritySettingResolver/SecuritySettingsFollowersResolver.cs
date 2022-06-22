7d19682517c18f50337309c8fd83b539b451506d
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-06-22 17:15:04 +0300

    implement new way of resolving

--- GhostNetwork.Gateway.Infrastructure/SecuritySettingResolver/SecuritySettingsFollowersResolver.cs
+++ GhostNetwork.Gateway.Infrastructure/SecuritySettingResolver/SecuritySettingsFollowersResolver.cs
@@ -1,28 +1,24 @@
 using Domain;
 using GhostNetwork.Gateway.SecuritySettings;
 using GhostNetwork.Gateway.Users;
-using GhostNetwork.Profiles.Api;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GhostNetwork.Gateway.Infrastructure.SecuritySettingResolver
 {
     public class SecuritySettingsFollowersResolver : ISecuritySettingsResolver
     {
+        private const string sectionName = "followers";
         private const string followersAccessMessage = "You cannot see followers of this user";
 
         private readonly ISecuritySettingStorage securitySettingStorage;
         private readonly ICurrentUserProvider currentUserProvider;
-        private readonly IRelationsApi relationsApi;
 
         public SecuritySettingsFollowersResolver(ISecuritySettingStorage securitySettingStorage,
-                                        ICurrentUserProvider currentUserProvider,
-                                        IRelationsApi relationsApi)
+                                        ICurrentUserProvider currentUserProvider)
         {
             this.securitySettingStorage = securitySettingStorage;
             this.currentUserProvider = currentUserProvider;
-            this.relationsApi = relationsApi;
         }
 
         public async Task<DomainResult> ResolveAccessAsync(Guid userId)
@@ -32,35 +28,9 @@ namespace GhostNetwork.Gateway.Infrastructure.SecuritySettingResolver
                 return DomainResult.Success();
             }
 
-            var setting = await securitySettingStorage.FindByProfileAsync(userId);
-
-            if (setting.Followers.Access == AccessLevel.NoOne)
+            if (!await securitySettingStorage.CheckAccessAsync(new Guid(currentUserProvider.UserId), userId, sectionName))
             {
                 return DomainResult.Error(followersAccessMessage);
-            }
-
-            if (setting.Followers.Access == AccessLevel.OnlyFriends)
-            {
-                if (!await relationsApi.IsFriendAsync(new Guid(currentUserProvider.UserId), friend: userId))
-                {
-                    return DomainResult.Error(followersAccessMessage);
-                }
-            }
-
-            if (setting.Followers.Access == AccessLevel.OnlyCertainUsers)
-            {
-                if (!setting.Followers.CertainUsers.Contains(new Guid(currentUserProvider.UserId)))
-                {
-                    return DomainResult.Error(followersAccessMessage);
-                }
-            }
-
-            if (setting.Followers.Access == AccessLevel.EveryoneExceptCertainUsers)
-            {
-                if (setting.Followers.CertainUsers.Contains(new Guid(currentUserProvider.UserId)))
-                {
-                    return DomainResult.Error(followersAccessMessage);
-                }
             }
 
             return DomainResult.Success();
