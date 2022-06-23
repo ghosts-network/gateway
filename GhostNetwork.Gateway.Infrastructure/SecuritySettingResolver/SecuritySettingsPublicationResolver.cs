a0cd3c5e4fe08ea90e2863d851db04b5cf7d0ffd
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-06-23 13:36:15 +0300

    Update SecuritySettingsPublicationResolver.cs

--- GhostNetwork.Gateway.Infrastructure/SecuritySettingResolver/SecuritySettingsPublicationResolver.cs
+++ GhostNetwork.Gateway.Infrastructure/SecuritySettingResolver/SecuritySettingsPublicationResolver.cs
@@ -1,28 +1,24 @@
 ﻿using Domain;
 using GhostNetwork.Gateway.SecuritySettings;
 using GhostNetwork.Gateway.Users;
-using GhostNetwork.Profiles.Api;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GhostNetwork.Gateway.Infrastructure.SecuritySettingResolver
 {
     public class SecuritySettingsPublicationResolver : ISecuritySettingsResolver
     {
+        private const string sectionName = "posts";
         private const string publicationAccessMessage = "You cannot see publications of this user";
 
         private readonly ISecuritySettingStorage securitySettingStorage;
         private readonly ICurrentUserProvider currentUserProvider;
-        private readonly IRelationsApi relationsApi;
 
         public SecuritySettingsPublicationResolver(ISecuritySettingStorage securitySettingStorage,
-                                        ICurrentUserProvider currentUserProvider,
-                                        IRelationsApi relationsApi)
+                                        ICurrentUserProvider currentUserProvider)
         {
             this.securitySettingStorage = securitySettingStorage;
             this.currentUserProvider = currentUserProvider;
-            this.relationsApi = relationsApi;
         }
 
         public async Task<DomainResult> ResolveAccessAsync(Guid userId)
@@ -32,35 +28,9 @@ namespace GhostNetwork.Gateway.Infrastructure.SecuritySettingResolver
                 return DomainResult.Success();
             }
 
-            var setting = await securitySettingStorage.FindByProfileAsync(userId);
-
-            if (setting.Posts.Access == AccessLevel.NoOne)
+            if (!await securitySettingStorage.CheckAccessAsync(new Guid(currentUserProvider.UserId), userId, sectionName))
             {
                 return DomainResult.Error(publicationAccessMessage);
-            }
-
-            if (setting.Posts.Access == AccessLevel.OnlyFriends)
-            {
-                if (!await relationsApi.IsFriendAsync(new Guid(currentUserProvider.UserId), friend: userId))
-                {
-                    return DomainResult.Error(publicationAccessMessage);
-                }
-            }
-
-            if (setting.Posts.Access == AccessLevel.OnlyCertainUsers)
-            {
-                if (!setting.Posts.CertainUsers.Contains(new Guid(currentUserProvider.UserId)))
-                {
-                    return DomainResult.Error(publicationAccessMessage);
-                }
-            }
-
-            if (setting.Posts.Access == AccessLevel.EveryoneExceptCertainUsers)
-            {
-                if (setting.Posts.CertainUsers.Contains(new Guid(currentUserProvider.UserId)))
-                {
-                    return DomainResult.Error(publicationAccessMessage);
-                }
             }
 
             return DomainResult.Success();
