df0641df36e9d704f163176db6a6e8a980a4de91
Alexander <50077233+Sekir0@users.noreply.github.com>
2022-08-12 15:47:53 +0300

    fix: create chat test

--- tests/GhostNetwork.Gateway.UnitTest/Chats/CreateChatAsyncTests.cs
+++ tests/GhostNetwork.Gateway.UnitTest/Chats/CreateChatAsyncTests.cs
@@ -25,6 +25,8 @@ public class CreateChatAsyncTests
 			Participants = new List<Guid>() {Guid.NewGuid(), Guid.NewGuid()}
 		};
 		
+		var currentUser = Guid.NewGuid();
+
 		var chat = new Chat(
 			"someId",
 			model.Name, 
@@ -35,13 +37,18 @@ public class CreateChatAsyncTests
 			});
 
 		var serviceMock = new Mock<IChatStorage>();
+		var currentUserMock = new Mock<ICurrentUserProvider>();
+		
 		serviceMock.Setup(x => x.CreateAsync(model.Name, model.Participants)).ReturnsAsync(chat);
+		currentUserMock.Setup(x => x.UserId).Returns(currentUser.ToString);
+		
 		
 		var client = TestServerHelper.New(collection =>
 		{
 			collection.AddAuthentication("Test")
 				.AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", _ => { });
 			collection.AddScoped(_ => serviceMock.Object);
+			collection.AddScoped(_ => currentUserMock.Object);
 		});
 		
 		// Act
