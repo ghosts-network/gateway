bab895cb3f6abee8310920f60430af99daebb225
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-08-24 16:05:49 +0300

    Create SecuritySettingsSectionInputModel.cs

---
+++ GhostNetwork.Gateway/Users/SecuritySection/SecuritySettingsSectionInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GhostNetwork.Gateway.Users.SecuritySection
+{
+    public class SecuritySettingsSectionInputModel
+    {
+        public AccessLevel Access { get; set; }
+
+        public IEnumerable<Guid> CertainUsers { get; set; }
+    }
+}
