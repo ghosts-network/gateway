e0e46afa83deb43a26255f1495c36592f37c0f41
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-09-25 13:39:28 +0300

    create models for graphQL

---
+++ GhostNetwork.Gateway.GraphQL/Models/PublicationEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace GhostNetwork.Gateway.GraphQL.Models
+{
+    public class PublicationEntity
+    {
+        public string Id { get; set; } = null!;
+
+        public string Content { get; set; } = string.Empty;
+
+        [GraphQLIgnore]
+        public string AuthorId { get; set; } = string.Empty;
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public DateTimeOffset? UpdatedOn { get; set; }
+    }
+}
---
+++ GhostNetwork.Gateway.GraphQL/Models/UserInfoEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace GhostNetwork.Gateway.GraphQL.Models
+{
+    public class UserInfoEntity
+    {
+        public string Id { get; set; } = null!;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string? AvatarUrl { get; set; }
+    }
+}
