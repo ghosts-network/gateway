46b0f65abc88e7e7b9d0e2e42692b507fa8e30c9
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-09-25 13:44:29 +0300

    Create NewsFeedPublicationQuery.cs

---
+++ GhostNetwork.Gateway.GraphQL/Queries/NewsFeedPublicationQuery.cs
@@ -0,0 +1,35 @@
+﻿using GhostNetwork.Content.Api;
+using GhostNetwork.Content.Model;
+using GhostNetwork.Gateway.GraphQL.Models;
+
+namespace GhostNetwork.Gateway.GraphQL.Queries
+{
+    public class NewsFeedPublicationQuery
+    {
+        private readonly IPublicationsApi publicationApi;
+        private readonly ICurrentUserProvider currentUserProvider;
+
+        public NewsFeedPublicationQuery(
+            ICurrentUserProvider currentUserProvider,
+            IPublicationsApi publicationApi)
+        {
+            this.publicationApi = publicationApi;
+            this.currentUserProvider = currentUserProvider;
+        }
+
+        [GraphQLName("publications")]
+        public async Task<IEnumerable<PublicationEntity>> GetPublicationsByUserIdAsync(int? take, string? cursor, Ordering? order, CancellationToken cancellationToken)
+        {
+            var publications = await publicationApi.SearchAsync(cursor, take, order: order, cancellationToken: cancellationToken);
+            
+            return publications.Select(p => new PublicationEntity
+            {
+                Id = p.Id,
+                Content = p.Content,
+                CreatedOn = p.CreatedOn,
+                UpdatedOn = p.UpdatedOn,
+                AuthorId = p.Author.Id.ToString()
+            });
+        }
+    }
+}
