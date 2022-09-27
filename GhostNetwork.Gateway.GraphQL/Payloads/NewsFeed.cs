19a35fded28bfd031f42afd51a5b82bea1c3cfa9
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2022-09-27 12:40:25 +0300

    Create NewsFeed.cs

---
+++ GhostNetwork.Gateway.GraphQL/Payloads/NewsFeed.cs
@@ -0,0 +1,11 @@
+﻿using GhostNetwork.Gateway.GraphQL.Models;
+
+namespace GhostNetwork.Gateway.GraphQL.Payloads
+{
+    public class NewsFeedPayload
+    {
+        public IEnumerable<PublicationEntity> Publications { get; set; } = Enumerable.Empty<PublicationEntity>();
+
+        public string? Cursor { get; set; }
+    }
+}
