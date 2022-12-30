03b1f9d76ac4666acb084cda8463908e2fce8c00
boberneprotiv <boberneprotiv@gmail.com>
2022-12-30 16:46:57 +0200

    add missed profile picture to relations

--- GhostNetwork.Gateway.Infrastructure/RestUserRelationsStorage.cs
+++ GhostNetwork.Gateway.Infrastructure/RestUserRelationsStorage.cs
@@ -3,7 +3,6 @@ using GhostNetwork.Gateway.Users;
 using GhostNetwork.Profiles.Api;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GhostNetwork.Gateway.Infrastructure
@@ -80,13 +79,13 @@ namespace GhostNetwork.Gateway.Infrastructure
             return DomainResult.Success();
         }
 
-        private async Task<IEnumerable<UserInfo>> GetProfilesByIdsAsync(IEnumerable<Guid> ids)
+        private async Task<IEnumerable<UserInfo>> GetProfilesByIdsAsync(IReadOnlyCollection<Guid> ids)
         {
-            var friends = new List<UserInfo>(ids.Count());
+            var friends = new List<UserInfo>(ids.Count);
             foreach (var id in ids)
             {
                 var friend = await profilesApi.GetByIdAsync(id);
-                friends.Add(new UserInfo(friend.Id, $"{friend.FirstName} {friend.LastName}", string.Empty));
+                friends.Add(new UserInfo(friend.Id, $"{friend.FirstName} {friend.LastName}", friend.ProfilePicture));
             }
 
             return friends;
